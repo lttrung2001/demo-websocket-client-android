c18223e1102b96bdc9c564b86af34a5d8a161756
unknown <lt.trung2001@gmail.com>
2023-03-03 13:12:36 +0700

    add record and playback

--- app/src/main/java/com/example/demowebsocket/MainActivity.kt
+++ app/src/main/java/com/example/demowebsocket/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.example.demowebsocket
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.*
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.demowebsocket.databinding.ActivityMainBinding
 import com.example.demowebsocket.models.PublishTopic
 import com.google.gson.Gson
 import okhttp3.*
 import okio.ByteString
+import okio.ByteString.Companion.toByteString
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var ws: WebSocket
+
+    private lateinit var audioTrack: AudioTrack
+    private lateinit var audioRecord: AudioRecord
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +40,15 @@ class MainActivity : AppCompatActivity() {
 
         val client = OkHttpClient()
         val request: Request = Request.Builder()
-            .url("ws://10.0.2.2:8080/websocket")
+            .url("ws://192.168.43.203:8080/websocket")
             .build()
 
         ws = client.newWebSocket(request, object : WebSocketListener() {
             override fun onOpen(webSocket: WebSocket, response: Response) {
                 // Connection opened
                 Log.i("INFO", "Connected")
+                initAudioTrack()
+                initAudioRecord()
             }
 
             override fun onMessage(webSocket: WebSocket, text: String) {
@@ -68,7 +79,71 @@ class MainActivity : AppCompatActivity() {
             override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
                 // Audio packet received
                 // Pass the audio packet to the media server for processing
+                // Convert bytes to PCM data
+                val pcmData = ShortArray(bytes.size)
+                ByteBuffer.wrap(bytes.toByteArray()).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer()
+                    .get(pcmData)
+
+                // Write PCM data to AudioTrack
+                audioTrack.write(pcmData, 0, pcmData.size)
             }
         })
+    }
+
+    private fun initAudioTrack() {
+        val sampleRate = 48000
+        val channelConfig = AudioFormat.CHANNEL_OUT_STEREO
+        val audioFormat = AudioFormat.ENCODING_PCM_16BIT
+        val bufferSize: Int =
+            AudioTrack.getMinBufferSize(sampleRate, channelConfig, audioFormat)
+        audioTrack = AudioTrack(
+            AudioManager.STREAM_MUSIC,
+            sampleRate,
+            channelConfig,
+            audioFormat,
+            bufferSize,
+            AudioTrack.MODE_STREAM
+        )
+
+        // Set AudioTrack parameters
+        audioTrack.setPlaybackRate(sampleRate)
+        audioTrack.setStereoVolume(1.0f, 1.0f)
+        audioTrack.play()
+    }
+
+    private fun initAudioRecord() {
+        val sampleRate = 44100
+        val channelConfig = AudioFormat.CHANNEL_IN_MONO
+        val audioFormat = AudioFormat.ENCODING_PCM_8BIT
+        val bufferSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat);
+        val buffer = ByteArray(bufferSize)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(arrayOf(Manifest.permission.RECORD_AUDIO), 1)
+        }
+        audioRecord = AudioRecord(
+            MediaRecorder.AudioSource.MIC,
+            sampleRate,
+            channelConfig,
+            audioFormat,
+            bufferSize
+        )
+        audioRecord.startRecording()
+
+        Thread {
+            while(true) {
+                val bytesRead = audioRecord.read(buffer, 0, bufferSize)
+                if (bytesRead != AudioRecord.ERROR_INVALID_OPERATION) {
+                    // process the bytes in the buffer array
+                    val audioBytes: ByteArray = buffer.copyOf(bytesRead)
+                    // send the audioBytes to server or use it for further processing
+                    ws.send(audioBytes.toByteString())
+                }
+            }
+        }.start()
     }
 }
